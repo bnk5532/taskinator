d632d593d8d60d3d84ae75ae19402eac5b5dceae
Brian King <bnk5532@gmail.com>
2022-06-24 17:35:34 -0400

    edit and delete buttons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -22,16 +23,42 @@ var taskFormHandler = function(event) {
 };
 
 var createTaskEl = function(taskDataObj) {
-    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     
     tasksToDoEl.appendChild(listItemEl);
-}
+
+    taskIdCounter++;
+};
+
+var createTaskActions = function(taskId) {
+var actionContainerEl = document.createElement("div");
+actionContainerEl.classname = "task-actions";
+
+var editButtonEl = document.createElement("button");
+editButtonEl.textContent = "Edit";
+editButtonEl.className = "btn edit-btn";
+editButtonEl.setAttribute("data-task-id", taskId);
+
+actionContainerEl.appendChild(editButtonEl);
+
+var deleteButtonEl = document.createElement("button");
+deleteButtonEl.textContent = "Delete";
+deleteButtonEl.className = "btn delete-btn";
+deleteButtonEl.setAttribute("data-task-id", taskId);
+
+actionContainerEl.appendChild(deleteButtonEl);
+
+return actionContainerEl;
+};
+
+
 
 formEl.addEventListener("submit", taskFormHandler); 
